8929848c64a0cdd90705390377bdaa999ee25448
Jane <262641236@qq.com>
2017-02-08 13:26:18 +0800

    dfdf

---
+++ test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  gzdai
+//
+//  Created by 简杰 on 2017/2/8.
+//  Copyright © 2017年 gzdai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : UIView
+
+@end
---
+++ test.m
@@ -0,0 +1,21 @@
+//
+//  test.m
+//  gzdai
+//
+//  Created by 简杰 on 2017/2/8.
+//  Copyright © 2017年 gzdai. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
